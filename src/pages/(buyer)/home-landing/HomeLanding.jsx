a77ea531ba4d7c40a44dc40b3c2093ac9f073742
ngocutn <ngocutn2003@gmail.com>
2024-10-04 23:13:36 +0700

    add banner

--- src/pages/(buyer)/home-landing/HomeLanding.jsx
+++ src/pages/(buyer)/home-landing/HomeLanding.jsx
@@ -1,8 +1,10 @@
+import Banner from "./components/Banner";
 import FeatureProduct from "./components/FeatureProduct";
 
 const HomeLanding = () => {
   return (
     <div className="mx-auto w-[80%]">
+      <Banner></Banner>
       <FeatureProduct></FeatureProduct>
     </div>
   );
