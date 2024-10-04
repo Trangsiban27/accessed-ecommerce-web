565d1a9dfa1c383ab06ad9c1df2419d872eda934
ngocutn <ngocutn2003@gmail.com>
2024-10-04 22:59:02 +0700

    feature product

--- src/pages/(buyer)/home-landing/HomeLanding.jsx
+++ src/pages/(buyer)/home-landing/HomeLanding.jsx
@@ -1,5 +1,10 @@
-const HomeLanding = () => {
-  return <div>Home Page</div>;
-};
+import FeatureProduct from "./components/FeatureProduct";
 
+const HomeLanding = () => {
+  return (
+    <div className="mx-auto w-[80%]">
+      <FeatureProduct></FeatureProduct>
+    </div>
+  );
+};
 export default HomeLanding;
