81874d0edda7221c0de707ab190d24021db52353
Trang Sĩ Bân <trangsiban4481@gmail.com>
2024-10-04 21:50:35 +0700

    update src

--- src/pages/(buyer)/product-detail/ProductDetail.jsx
+++ src/pages/(buyer)/product-detail/ProductDetail.jsx
@@ -2,7 +2,7 @@ import ProductImage from "./components/product-image/ProductImage";
 
 const ProductDetail = () => {
   return (
-    <div className="w-full max-w-[1440px] mt-10">
+    <div className="w-full max-w-[100%] mt-10">
       <ProductImage></ProductImage>
     </div>
   );
