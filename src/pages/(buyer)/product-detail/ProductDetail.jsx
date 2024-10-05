49be0ebeb033b0359aeaf8ec891059a7b959af6f
Trang Sĩ Bân <trangsiban4481@gmail.com>
2024-10-05 08:42:28 +0700

    add homelanding

--- src/pages/(buyer)/product-detail/ProductDetail.jsx
+++ src/pages/(buyer)/product-detail/ProductDetail.jsx
@@ -3,7 +3,7 @@ import ProductInfor from "./components/product-infor/ProductInfor";
 
 const ProductDetail = () => {
   return (
-    <div className="w-full max-w-[100%] mt-10 flex gap-x-8">
+    <div className="w-[80%] mt-10 flex gap-x-8 mx-auto">
       <ProductImage></ProductImage>
       <ProductInfor></ProductInfor>
     </div>
