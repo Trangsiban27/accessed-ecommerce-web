bdd052966352f7701b7055345b7dcbd620bcfe26
tranminhhieu212 <minhhieu8112003@gmail.com>
2024-10-19 10:01:38 +0700

    cmt

--- src/pages/(seller)/add-product/AddProduct.jsx
+++ src/pages/(seller)/add-product/AddProduct.jsx
@@ -15,7 +15,6 @@ const AddProduct = () => {
   return (
     <div className="flex flex-col items-center justify-start w-full max-w-[1440px] h-full p-3 mx-auto">
       <div className="w-full h-full my-2 scrollBar">
-        {/* <SubHeader /> */}
         <div className="w-full flex items-start justify-center p-2 gap-3">
           <div className="w-1/2 flex flex-col items-center justify-start">
             <ProdCategory />
@@ -35,7 +34,6 @@ const AddProduct = () => {
         </div>
         <ProdVariants />
         <ProdVariantTable />
-        {/*     <ActionButtons /> */}
       </div>
     </div>
   );
